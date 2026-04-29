13146ca95189a93115c34c98bb26f6dbd1a25e0d
Mykhailo Trofimtsov <mykhailo.trofimtsov@sigma.software>
2026-04-29 03:43:29 +0300

    Code base

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const userRouter = require('./user/user.router');
+const productRouter = require('./product/product.router');
+const { logRequest } = require('./middleware');
+const bodyParser = require('body-parser');
+const { errorResponder } = require('./error.middleware');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(logRequest);
+app.use(productRouter);
+app.use(userRouter);
+app.use(errorResponder);
+
+app.listen(PORT, () => {
+   console.log(`Server listening at http://localhost:${PORT}`);
+});
